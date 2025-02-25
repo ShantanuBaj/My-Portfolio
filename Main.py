38a9f034c618a099deb881e8c01193f6b14dfe94
Shan <shantanu007.sb@gmail.com>
2025-02-25 17:05:48 +0530

    Created columns, image and description

---
+++ Main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2= st.columns(2)
+
+with col1:
+    st.image("stadium1.png")
+
+with col2:
+    st.title("Shantanu Bajpai")
+    content= """Dedicated and results-driven professional with extensive experience in IP Service Operations and HR processes. Proven ability to
+manage complex data, optimize processes, and ensure high-quality client and employee support. Adept at collaborating with
+cross-functional teams to drive data-driven decision-making and process improvements."""
+    st.info(content)
