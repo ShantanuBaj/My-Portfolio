242e3a6613f44fc73a04ab95b760665e43185ee4
Shan <shantanu007.sb@gmail.com>
2025-03-03 16:00:45 +0530

    Day 23: Send email using the form.

--- pages/Contact_me.py
+++ pages/Contact_me.py
@@ -1,17 +1,23 @@
 import streamlit as st
 from send_email import send_email
+import pandas
 
-st.header("Contact me")
+df= pandas.read_csv("topics.csv")
 
 with st.form(key="email_form"):
-    user_email=st.text_input("Your Email Address")
-    raw_message= st.text_area("Your message")
-    message= f"""\
+    user_email = st.text_input("Your Email Address")
+    option= st.selectbox(
+        'What topic do you want to discuss?',
+        df["topic"])
+    raw_message = st.text_area("Your message")
+    message = f"""\
 Subject: New email from {user_email}
 
 From: {user_email}
+Topic {option}
 {raw_message}
 """
+
     button= st.form_submit_button("Submit")
     print(button)
     if button:
