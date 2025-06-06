18b655c4e404754f3b66c4dde1c416ca8492ee0f
Shan <shantanu007.sb@gmail.com>
2025-06-06 10:23:24 +0530

    Day 23: Send email using the form.

--- Home.py
+++ Home.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import pandas
+import os
+
+current_dir = os.path.dirname(__file__)
+image_path = os.path.join(current_dir, "stadium1.png")
 
 st.set_page_config(layout="wide")
 
 col1, col2= st.columns(2)
+
 
 with col1:
     st.image("stadium1.png", width=300)
